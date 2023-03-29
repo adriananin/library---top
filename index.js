f6df37865b8efe377862d42ff04e486bf8a3ae69
adriananin <113365066+adriananin@users.noreply.github.com>
2023-03-29 23:12:12 +0000

    Dynamic Form added in JS, Styling updated

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const bookContainer = document.querySelector(".card--container");
+const addFormBtn = document.querySelector(".addbook");
+addFormBtn.addEventListener("click", toggleContainerDiv);
+addFormBtn.addEventListener("click", createForm);
+
 function Book(title, author, pages, read) {
   (this.title = title),
     (this.author = author),
@@ -9,94 +14,131 @@ Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 };
 
-// const alchemist = new Book(
-//   "The Alchemist",
-//   "Paulo Coelho",
-//   "288",
-//   "has been read"
-// );
+let myLibrary = [];
 
-// console.log(alchemist.info());
+function toggleContainerDiv() {
+  const div = document.querySelector(".form--container");
+  div.style.display = "flex";
+}
 
-let myLibrary = [
-  { title: "ephesians", author: "paul", pages: 3, read: "yes" },
-  { title: "acts", author: "luke", pages: 16, read: "yes" },
-  { title: "genesis", author: "moses", pages: 21, read: "yes" },
-];
+function createForm() {
+  const form = document.createElement("form");
+  form.classList.add("centered-form");
+
+  const formTitle = document.createElement("h2");
+  formTitle.textContent = "Add New Book";
+  formTitle.classList.add("formtitle");
+
+  const title = document.createElement("input");
+  title.classList.add("input");
+  title.type = "text";
+  title.placeholder = "The book title";
+  title.id = "title";
+
+  const author = document.createElement("input");
+  author.classList.add("input");
+  author.type = "text";
+  author.placeholder = "The book author";
+  author.id = "author";
+
+  const pages = document.createElement("input");
+  pages.classList.add("input");
+  pages.type = "number";
+  pages.placeholder = "Number of pages";
+  pages.id = "pages";
+
+  const checkboxDiv = document.createElement("div");
+  checkboxDiv.classList.add("checkBoxDiv");
+  const checkbox = document.createElement("input");
+  checkbox.classList.add("input");
+  checkbox.type = "checkbox";
+  checkbox.id = "checkbox";
+  const checkboxLabel = document.createElement("label");
+  checkboxLabel.classList.add("label");
+  checkboxLabel.htmlFor = "checkbox";
+  checkboxLabel.appendChild(document.createTextNode("Have you read it ?"));
+  checkboxDiv.appendChild(checkbox);
+  checkboxDiv.appendChild(checkboxLabel);
+
+  const submit = document.createElement("button");
+  submit.classList.add("submitBtn");
+  submit.innerHTML = "Submit";
+
+  form.appendChild(formTitle);
+  form.appendChild(title);
+  form.appendChild(author);
+  form.appendChild(pages);
+  form.appendChild(checkboxDiv);
+  form.appendChild(submit);
+
+  submit.addEventListener("click", displayBook);
+  submit.addEventListener("click", addBookToLibrary);
+
+  const formCon = document.querySelector(".form--container");
+  formCon.appendChild(form);
+}
+
+const removeEventListeners = () => {
+  submit.removeEventListener("click", displayBook);
+  submit.removeEventListener("click", addBookToLibrary);
+};
+
+function hideForm() {
+  const hide = document.querySelector(".form--container");
+  hide.style.display = "none";
+  removeEventListeners();
+}
 
 function addBookToLibrary() {
-  let title = prompt("Enter the title of the book:");
-  let author = prompt("Enter the author of the book:");
-  let pages = prompt("Enter the number of pages in the book:");
-  let read = prompt("Have you read the book? (yes or no)");
-  let alchemist = new Book(title, author, pages, read);
-  myLibrary.push(alchemist);
+  bookContainer.innerHTML = ""; // clear existing book cards
+
+  const newBookFormElem = document.querySelector(".centered-form");
+  newBookFormElem.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const newTitle = document.querySelector("#title").value;
+    const newAuthor = document.querySelector("#author").value;
+    const newPages = document.querySelector("#pages").value;
+    const newRead = document.querySelector("#checkbox").checked;
+
+    myLibrary.push(new Book(newTitle, newAuthor, newPages, newRead));
+
+    const formDisplay = document.querySelector(".form--container");
+    formDisplay.innerHTML = "";
+
+    displayBook();
+    hideForm();
+  });
 }
 
 function displayBook() {
   for (const book of myLibrary) {
-    console.log(`${book.title} by ${book.author}`);
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("card");
+    const titleElement = document.createElement("p");
+    titleElement.textContent = book.title;
+
+    const authorElement = document.createElement("p");
+    authorElement.textContent = book.author;
+
+    const pagesElement = document.createElement("p");
+    pagesElement.textContent = `${book.pages} pages`;
+
+    const readElement = document.createElement("p");
+    readElement.textContent = book.read ? "Read" : "Not read yet";
+
+    const toggleReadBtn = document.createElement("button");
+    toggleReadBtn.id = "read";
+    toggleReadBtn.textContent = book.read ? "Mark as Unread" : "Mark as Read";
+    toggleReadBtn.addEventListener("click", function () {
+      book.read = !book.read;
+    });
+
+    bookDiv.appendChild(titleElement);
+    bookDiv.appendChild(authorElement);
+    bookDiv.appendChild(pagesElement);
+    bookDiv.appendChild(readElement);
+    bookDiv.appendChild(toggleReadBtn);
+    bookContainer.appendChild(bookDiv);
   }
-}
-
-// addBookToLibrary();
-
-// displayBook();
-
-const addFormBtn = document.querySelector(".buttoncard");
-addFormBtn.addEventListener("click", createForm);
-
-function createForm() {
-  // Create form element
-  const form = document.createElement("form");
-  form.classList.add("centered-form");
-
-  // Create input fields
-  const input1 = document.createElement("input");
-  input1.type = "text";
-  input1.placeholder = "Input 1";
-
-  const input2 = document.createElement("input");
-  input2.type = "text";
-  input2.placeholder = "Input 2";
-
-  const input3 = document.createElement("input");
-  input3.type = "text";
-  input3.placeholder = "Input 3";
-
-  // Create checkbox
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.id = "checkbox";
-  const checkboxLabel = document.createElement("label");
-  checkboxLabel.htmlFor = "checkbox";
-  checkboxLabel.appendChild(document.createTextNode("Checkbox"));
-
-  // Add inputs and checkbox to form
-  form.appendChild(input1);
-  form.appendChild(input2);
-  form.appendChild(input3);
-  form.appendChild(checkbox);
-  form.appendChild(checkboxLabel);
-
-  // Add form to page
-
-  const formCon = document.querySelector("body");
-  formCon.appendChild(form);
-
-  // const formContainer = document.querySelector(".form--container");
-  // formContainer.appendChild(form);
-
-  // const formOverlay = document.querySelector(".form-overlay");
-  // formOverlay.classList.add("overlay");
-  // const overlay = document.createElement("div");
-  // overlay.id = "overlay";
-  // overlay.style.position = "fixed";
-  // overlay.style.top = "0";
-  // overlay.style.left = "0";
-  // overlay.style.width = "100%";
-  // overlay.style.height = "100%";
-  // overlay.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
-  // overlay.style.zIndex = "9998";
-  // document.body.appendChild(overlay);
 }
